878fe027da90fb4e1161cd0af33290c73341a5df
Vincent Huang <40749774+vsai12@users.noreply.github.com>
2021-04-11 22:55:34 -0700

    Update vhuan025_lab2_part4.c

--- turnin/vhuan025_lab2_part4.c
+++ turnin/vhuan025_lab2_part4.c
@@ -21,12 +21,8 @@ int main(void) {
         while (1) {
 		unsigned char sum = 0x00; //varaible to hold sum weight of carts
 		unsigned char tmpD = 0x00; //temp var to hold value of D
-		if(PINA > 0x8C) //checks if cartA > 140
+		if((PINA + PINB + PINC) > 0x8C) //checks if carts' weight > 140
 			tmpD = tmpD | 0x01; //sets PD0 to 1;
-		else if(PINB > 0x8C) //checks if cartB > 140
-                        tmpD = tmpD | 0x01; //sets PD0 to 1;
-		else if(PINC > 0x8C) //checks if cartC > 140
-                        tmpD = tmpD | 0x01; //sets PD0 to 1;
 
 		if(PINA > PINC) {
 			if(PINA - PINC > 0x50)
@@ -37,6 +33,7 @@ int main(void) {
 			tmpD = tmpD | 0x02; //sets PD1 to 1
 		
 		sum = PINA + PINB + PINC;
+		sum = sum >> 2; //shift sum right 2
 		sum = sum & 0xFC; //sets last 2 bits of sum to 0
 		tmpD = sum | tmpD; //copies the first 6 bits of sum into tmpD
 		PORTD = tmpD;
